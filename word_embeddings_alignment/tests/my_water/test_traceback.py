b4ecad6b3500144c1364986f3696b4ccd1c5f239
wojciech-galan <wojciech.galan@gmail.com>
2023-03-02 16:51:15 +0100

    tests for traceback function

--- word_embeddings_alignment/tests/my_water/test_traceback.py
+++ word_embeddings_alignment/tests/my_water/test_traceback.py
@@ -25,7 +25,7 @@ def test_warning():
 
 
 def test_mathing_sequences():
-	t = traceback(
+	a = traceback(
 		np.array([
 			[0, 0, 0, 0],
 			[0, 5, 0, 0],
@@ -40,6 +40,56 @@ def test_mathing_sequences():
 		], dtype=np.byte),
 		'ACG', 'ACG'
 	)
-	assert t.score == 15
-	assert t.seq1 == 'ACG'
-	assert t.seq2 == 'ACG'
+	assert a.score == 15
+	assert a.seq1 == 'ACG'
+	assert a.seq2 == 'ACG'
+
+
+def test_1_linear_gap(linear_gap__distance_matrix_1: np.ndarray, linear_gap__traceback_matrix_1: np.ndarray):
+	a = traceback(
+		linear_gap__distance_matrix_1,
+		(6, 6),
+		linear_gap__traceback_matrix_1,
+		'AAAAAA', 'AATATA'
+	)
+	assert a.score == 12
+	assert a.seq1 == 'AAAAAA'
+	assert a.seq2 == 'AATATA'
+
+
+def test_2_linear_gap(linear_gap__distance_matrix_2: np.ndarray, linear_gap__traceback_matrix_2: np.ndarray):
+	a = traceback(
+		linear_gap__distance_matrix_2,
+		(7, 7),
+		linear_gap__traceback_matrix_2,
+		'AAATAAA', 'AATATAA'
+	)
+	assert a.score == 29
+	assert a.seq1 == 'AATA-AA'
+	assert a.seq2 == 'AATATAA'
+
+
+def test_3_linear_gap(linear_gap__distance_matrix_3: np.ndarray, linear_gap__traceback_matrix_3: np.ndarray):
+	with pytest.warns(MultipleEquallyScoredPathsFromMaxTo0, match="Multiple best-scoring alignments are possible"):
+		a = traceback(
+			linear_gap__distance_matrix_3,
+			(5, 8),
+			linear_gap__traceback_matrix_3,
+			'CGCAT', 'CGCCGTAT'
+		)
+	assert a.score == 22
+	assert a.seq1 == 'CGC---AT'
+	assert a.seq2 == 'CGCCGTAT'
+
+
+def test_1_affine_gap(affine_gap__distance_matrix_1: np.ndarray, affine_gap__traceback_matrix_1: np.ndarray):
+	with pytest.warns(MultipleEquallyScoredPathsFromMaxTo0, match="Multiple best-scoring alignments are possible"):
+		a = traceback(
+			affine_gap__distance_matrix_1,
+			(5, 8),
+			affine_gap__traceback_matrix_1,
+			'CGCAT', 'CGCCGTAT'
+		)
+	assert a.score == 18
+	assert a.seq1 == 'CGC---AT'
+	assert a.seq2 == 'CGCCGTAT'
