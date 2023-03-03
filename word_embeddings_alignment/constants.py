10a41ed149ab870357f63294d3e681220ec41933
wojciech-galan <wojciech.galan@gmail.com>
2023-03-03 18:53:15 +0100

    word embeddings for proteins

---
+++ word_embeddings_alignment/constants.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+_this_dir = os.path.dirname(__file__)
+PACKAGE_DIR = os.path.split(_this_dir)[0]
+_conf_path = os.path.join(PACKAGE_DIR, 'conf.json')
+CONF = json.load(open(_conf_path))
+PROT_VEC_CSV = os.path.join(PACKAGE_DIR, CONF['prot_dir'], CONF['prot_file'])
+PROT_VEC_PICKLE = os.path.join(PACKAGE_DIR, CONF['prot_dir'], CONF['prot_file_serialized'])
---
+++ word_embeddings_alignment/data_transformation/read_prot_vec.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import constants
+import pickle
+from typing import Dict
+
+
+def read(f_name=constants.PROT_VEC_CSV) -> Dict[str, np.ndarray]:
+    ret_dir = {}
+    with open(f_name) as f:
+        for line in f:
+            k, v = line.rstrip().strip('"').split(None, 1)
+            v = np.array([float(x) for x in v.split()])
+            ret_dir[k] = v
+    return ret_dir
+
+
+if __name__ == '__main__':
+    prot_vecs = read()
+    with open(constants.PROT_VEC_PICKLE, 'wb') as outfile:
+        pickle.dump(prot_vecs, outfile)
