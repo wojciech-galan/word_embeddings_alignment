c87e7765a5f4210c33d5664920addf53c37d58a7
wojciech-galan <wojciech.galan@gmail.com>
2023-03-10 13:50:34 +0100

    three additional tests for `align` function in my_water

--- word_embeddings_alignment/tests/my_water/test_align.py
+++ word_embeddings_alignment/tests/my_water/test_align.py
@@ -3,6 +3,8 @@ import pytest
 from typing import Dict
 
 from word_embeddings_alignment.regular_water.my_water import align
+from word_embeddings_alignment.my_warnings import MultipleEquallyScoredPathsFromMaxTo0
+from word_embeddings_alignment.my_warnings import MultipleMaxValuesInDistanceMatrix
 
 
 def test_not_similar(ednafull_simplified: Dict[str, Dict[str, int]]):
@@ -21,3 +23,33 @@ def test_the_same(ednafull_simplified: Dict[str, Dict[str, int]]):
 	assert a.score == 15
 	assert a.seq1 == 'ACG'
 	assert a.seq2 == 'ACG'
+
+
+def test_affine_gap_penalty(ednafull_simplified: Dict[str, Dict[str, int]]):
+	with pytest.warns(MultipleEquallyScoredPathsFromMaxTo0):
+		a = align(
+			'CGCAT', 'CGCCGTAT', ednafull_simplified, 5, 1
+		)
+	assert a.score == 18
+	assert a.seq1 == 'CGC---AT'
+	assert a.seq2 == 'CGCCGTAT'
+
+
+def test_2_affine_gap_penalty(ednafull_simplified: Dict[str, Dict[str, int]]):
+	with pytest.warns(MultipleEquallyScoredPathsFromMaxTo0):
+		a = align(
+			'ATGGCCTC', 'ACGGCTC', ednafull_simplified, 5, 1
+		)
+	assert a.score == 21
+	assert a.seq1 == 'ATGGCCTC'
+	assert a.seq2 == 'ACGG-CTC'
+
+
+def test_3_affine_gap_penalty(ednafull_simplified: Dict[str, Dict[str, int]]):
+	with pytest.warns(MultipleMaxValuesInDistanceMatrix):
+		a = align(
+			'ATGGCCTC', 'ACGGCTC', ednafull_simplified, 10, 1
+		)
+	assert a.score == 16
+	assert a.seq1 == 'ATGGC'
+	assert a.seq2 == 'ACGGC'
