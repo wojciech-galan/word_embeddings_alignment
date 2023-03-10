4c088a4ce50df0a66ecc85030a76422ab5f9ff09
wojciech-galan <wojciech.galan@gmail.com>
2023-03-10 10:57:15 +0100

    simple tests for create_distance_and_traceback_matrices

---
+++ word_embeddings_alignment/tests/word_embeddings_water/test_create_distance_and_traceback_matrices.py
@@ -0,0 +1,118 @@
+import pytest
+import numpy as np
+from word_embeddings_alignment.word_embeddings_water.word_embeddings_water import create_distance_and_traceback_matrices
+
+
+@pytest.fixture()
+def embeddings() -> np.ndarray:
+	return {
+		'ACG': np.array([0, 0]),
+		'CGA': np.array([0, 3]),
+		'CGT': np.array([0, 4])
+	}
+
+
+def set_up(mocker):
+	mocked_points_for_word_embeddings = mocker.patch(
+		'word_embeddings_alignment.word_embeddings_water.word_embeddings_water.points_for_word_embeddings')
+	mocked_points_for_word_embeddings.return_value = 45
+	mocked_get_first_key_from_a_dict = mocker.patch(
+		'word_embeddings_alignment.word_embeddings_water.word_embeddings_water.get_first_key_from_a_dict')
+	mocked_get_first_key_from_a_dict.return_value = '___'
+
+
+def test_short_aligned_no_identical_nucleotides(embeddings: np.ndarray, mocker):
+	set_up(mocker)
+	dm, tm = create_distance_and_traceback_matrices(
+		'ACG',
+		'ACG',
+		None,
+		5, 5
+	)
+	dm_template = np.array([
+		[0, 0, 0,  0],
+		[0, 0, 0,  0],
+		[0, 0, 0,  0],
+		[0, 0, 0, 45]
+	])
+	np.testing.assert_array_equal(dm, dm_template)
+	tm_template = np.array([
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 4]
+	])
+	np.testing.assert_array_equal(tm, tm_template)
+
+
+def test_short_aligned_4_identical_nucleotides(embeddings: np.ndarray, mocker):
+	set_up(mocker)
+	dm, tm = create_distance_and_traceback_matrices(
+		'AAAA',
+		'AAAA',
+		None,
+		5, 5
+	)
+	dm_template = np.array([
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0, 45, 45],
+		[0, 0, 0, 45, 45]
+	])
+	np.testing.assert_array_equal(dm, dm_template)
+	tm_template = np.array([
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 4, 4],
+		[0, 0, 4, 4]
+	])
+	np.testing.assert_array_equal(tm, tm_template)
+
+
+def test_short_aligned_4_matching_nucleotides(embeddings: np.ndarray, mocker):
+	set_up(mocker)
+	dm, tm = create_distance_and_traceback_matrices(
+		'ACGA',
+		'ACGA',
+		None,
+		5, 5
+	)
+	dm_template = np.array([
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0, 45, 45],
+		[0, 0, 0, 45, 45]
+	])
+	np.testing.assert_array_equal(dm, dm_template)
+	tm_template = np.array([
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 4, 4],
+		[0, 0, 4, 4]
+	])
+	np.testing.assert_array_equal(tm, tm_template)
+
+
+def test_short_aligned_4_partially_matching_nucleotides_no_mocks(embeddings: np.ndarray):
+	dm, tm = create_distance_and_traceback_matrices(
+		'ACGA',
+		'ACGT',
+		embeddings,
+		5, 5
+	)
+	dm_template = np.array([
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0,  0,  0],
+		[0, 0, 0, 45, 40],
+		[0, 0, 0, 40, 35]
+	])
+	np.testing.assert_array_equal(dm, dm_template)
+	tm_template = np.array([
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 4, 18],
+		[0, 0, 9, 27]
+	])
+	np.testing.assert_array_equal(tm, tm_template)
--- word_embeddings_alignment/word_embeddings_water/word_embeddings_water.py
+++ word_embeddings_alignment/word_embeddings_water/word_embeddings_water.py
@@ -1,6 +1,10 @@
+import warnings
 from typing import Tuple
 from typing import Dict
 from typing import SupportsFloat, Union
+from word_embeddings_alignment.SimpleAlignmentRepresentation import SimpleAlignmentRepresentation
+from word_embeddings_alignment.my_warnings import MultipleEquallyScoredPathsFromMaxTo0
+from word_embeddings_alignment.my_warnings import MultipleMaxValuesInDistanceMatrix
 
 import numpy as np
 
@@ -16,33 +20,96 @@ GAP = '-'
 
 def create_distance_and_traceback_matrices(seq_a: str, seq_b: str, word_embeddings: Dict[str, np.ndarray],
                                            gap_open: Numeric, gap_extend: Numeric) -> Tuple[np.ndarray, np.ndarray]:
-	word_length = get_first_key_from_a_dict(word_embeddings)
+	word_length = len(get_first_key_from_a_dict(word_embeddings))
+	if word_length != 3:
+		raise NotImplementedError
 	# create initial matrix
-	distance_matrix = np.full((len(seq_a) + 1, len(seq_b) + 1), np.NaN)
+	distance_matrix = np.zeros((len(seq_a) + 1, len(seq_b) + 1))
 	traceback_matrix = np.zeros((len(seq_a), len(seq_b)), dtype=np.byte)
-	# initialize first row and column
-	distance_matrix[0, :] = 0
-	distance_matrix[:, 0] = 0
 	# fill the matrix
 	for i, char_a in enumerate(seq_a, 1):
 		for j, char_b in enumerate(seq_b, 1):
 			# gap_penalty equals gap_extend if there is already a gap in a previous cell, else gap_open
-			if (i + 3 <= len(seq_a)) and (j + 3 <= len(seq_b)):
+			if (i + 2 <= len(seq_a)) and (j + 2 <= len(seq_b)):
 				to_subtract = min(max(i, j), 3)
-				slant = distance_matrix[i - to_subtract, j - to_subtract] + euclidean_distance(word_embeddings[seq_a[i:i+3]], word_embeddings[seq_b[j:j+3]])
+				slant = distance_matrix[i - to_subtract, j - to_subtract] + points_for_word_embeddings(word_embeddings, seq_a[i-1:i+2], seq_b[j-1:j+2])
 			else:
-				slant = np.inf
+				slant = 0
 			upper = distance_matrix[i - 1, j] - (
 				gap_extend if (
 						i > 2 and j > 1 and traceback_matrix[i - 2, j - 1] & GAP_OPENED_FROM_UPPER) else gap_open)
 			left = distance_matrix[i, j - 1] - (
 				gap_extend if (i > 1 and j > 2 and traceback_matrix[i - 1, j - 2] & GAP_OPENED_FROM_LEFT) else gap_open)
-			minimum = min(slant, upper, left, np.inf)
-			distance_matrix[i, j] = minimum
-			traceback_matrix[i - 1, j - 1] |= (UPPER | GAP_OPENED_FROM_UPPER if minimum == upper else 0) | \
-			                                 (LEFT | GAP_OPENED_FROM_LEFT if minimum == left else 0) | \
-			                                 (SLANT if minimum == slant else 0)
+			maximum = max(slant, upper, left, 0)
+			print(i, j, slant, upper, left)
+			# import pdb
+			# if i == 4 and j == 4:
+			# 	pdb.set_trace()
+			if maximum == 0:
+				pass
+			elif maximum == slant:
+				traceback_matrix[i + 1, j + 1] |= SLANT
+				distance_matrix[i+2, j+2] = slant
+			# in the code below: maximum == upper or maximum == left
+			elif traceback_matrix[i - 1, j - 1] == SLANT and distance_matrix[i, j] > maximum:
+				pass
+			elif traceback_matrix[i - 1, j - 1] == SLANT and distance_matrix[i, j] == maximum:
+				traceback_matrix[i - 1, j - 1] |= UPPER | GAP_OPENED_FROM_UPPER if maximum == upper else 0
+				traceback_matrix[i - 1, j - 1] |= LEFT | GAP_OPENED_FROM_LEFT if maximum == left else 0
+			elif (traceback_matrix[i - 1, j - 1] == SLANT and distance_matrix[i, j] < maximum) \
+					or \
+				 (traceback_matrix[i - 1, j - 1] == 0):
+				distance_matrix[i, j] = maximum
+				traceback_matrix[i - 1, j - 1] = UPPER | GAP_OPENED_FROM_UPPER if maximum == upper else 0
+				traceback_matrix[i - 1, j - 1] |= LEFT | GAP_OPENED_FROM_LEFT if maximum == left else 0
+			else:
+				# should never happen
+				pdb.set_trace()
+
+			print(distance_matrix)
+			print(traceback_matrix)
 	return distance_matrix, traceback_matrix
+
+
+def traceback(distance_matrix: np.ndarray, max_element_indices: Tuple[int, int], traceback_matrix: np.ndarray,
+              seq_a: str, seq_b: str):
+	element = distance_matrix[max_element_indices]
+	previously_used = 0
+	curr_element_a, curr_element_b = max_element_indices
+	alignment = SimpleAlignmentRepresentation(element)
+	while element:
+		direction = traceback_matrix[curr_element_a - 1, curr_element_b - 1]
+		if direction & (SLANT | UPPER | LEFT) in AMBIGUOUS_DIRECTIONS:
+			warnings.warn("Multiple best-scoring alignments are possible", MultipleEquallyScoredPathsFromMaxTo0)
+		# first check, whether we are already extending a gap
+		if (previously_used == LEFT) and (direction & LEFT):
+			# gap on the first sequence
+			alignment.add_data(GAP, seq_b[curr_element_b - 1])
+			curr_element_b -= 1
+			previously_used = LEFT
+		elif (previously_used == UPPER) and (direction & UPPER):
+			# gap on the second sequence
+			alignment.add_data(seq_a[curr_element_a - 1], GAP)
+			curr_element_a -= 1
+			previously_used = UPPER
+		# if we are not extending a gap:
+		elif direction & SLANT:
+			alignment.add_data(seq_a[curr_element_a - 4:curr_element_a-1], seq_b[curr_element_b - 4:curr_element_b-1])
+			curr_element_a -= 4
+			curr_element_b -= 4
+			previously_used = 0
+		elif direction & LEFT:
+			# gap on the first sequence
+			alignment.add_data(GAP, seq_b[curr_element_b - 1])
+			curr_element_b -= 1
+			previously_used = LEFT
+		elif direction & UPPER:
+			# gap on the second sequence
+			alignment.add_data(seq_a[curr_element_a - 1], GAP)
+			curr_element_a -= 1
+			previously_used = UPPER
+		element = distance_matrix[curr_element_a, curr_element_b]
+	return alignment
 
 
 def get_first_key_from_a_dict(a_dict: Dict):
@@ -53,4 +120,17 @@ def euclidean_distance(vector1: np.ndarray, vector2: np.ndarray):
 	return np.linalg.norm(vector1-vector2)
 
 
-print(euclidean_distance(np.array([3, 2]), np.array([4, 1])))
+def points_for_word_embeddings(word_embeddings: Dict[str, np.ndarray], seq1: str, seq2: str):
+	return 60/(1 + euclidean_distance(word_embeddings[seq1],  word_embeddings[seq2])) - 15
+
+
+if __name__ == '__main__':
+	from word_embeddings_alignment.data_transformation import read_prot_vec
+	# print(create_distance_and_traceback_matrices('ACG', 'ACG', {'ACG':np.array([0, 1])}, 5, 5))
+	# print(points_for_word_embeddings(read_prot_vec.read(), 'CGA', 'ACG'))
+	create_distance_and_traceback_matrices(
+		'ACGA',
+		'ACGT',
+		read_prot_vec.read(),
+		5, 5
+	)
