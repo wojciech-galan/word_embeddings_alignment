42924b148fddfe8164ac67b96812ff8e0e824086
wojciech-galan <wojciech.galan@gmail.com>
2023-03-15 13:52:18 +0100

    fixed PACKAGE_DIR

--- word_embeddings_alignment/src/constants.py
+++ word_embeddings_alignment/src/constants.py
@@ -2,7 +2,7 @@ import os
 import json
 
 _this_dir = os.path.dirname(__file__)
-PACKAGE_DIR = os.path.split(_this_dir)[0]
+PACKAGE_DIR = _this_dir.rsplit(os.path.sep, 2)[0]
 _conf_path = os.path.join(PACKAGE_DIR, 'conf.json')
 CONF = json.load(open(_conf_path))
 PROT_VEC_CSV = os.path.join(PACKAGE_DIR, CONF['prot_dir'], CONF['prot_file'])
